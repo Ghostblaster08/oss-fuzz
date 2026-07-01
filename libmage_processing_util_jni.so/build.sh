e90bd82adf15dbf72161c067e9c6e2c460a31866
Jai Desar <141903653+Ghostblaster08@users.noreply.github.com>
2026-07-01 12:58:59 +0530

    Update build script to copy fuzzer and .so files
    Added commands to copy fuzzer and pre-compiled .so files to output directory.

--- libmage_processing_util_jni.so/build.sh
+++ libmage_processing_util_jni.so/build.sh
@@ -6,3 +6,9 @@
     export NDK=/src/android-ndk-r25c
   
     ./build.sh afl
+  
+    # Copy the fuzzer to the output directory
+    cp ../build/fuzz_image_processing_afl $OUT/fuzz_image_processing
+  
+    # Copy the pre-compiled .so since the harness dlopens it
+    cp ../lib/libimage_processing_util_jni.so $OUT/
