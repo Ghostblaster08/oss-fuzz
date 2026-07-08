8b3e84d098e014b4a1fea53e6c40ca1eff0c3c6c
Jai Desar <141903653+Ghostblaster08@users.noreply.github.com>
2026-07-08 10:39:11 +0530

    Create build directory in build script
    Added a line to create the build directory before building the fuzzer.

--- projects/libmage_processing_util_jni.so/build.sh
+++ projects/libmage_processing_util_jni.so/build.sh
@@ -1,16 +1,17 @@
-    #!/bin/bash -eu                                                                                                                                                                  
-                                                                                                                                                                                     
-    # Build the ARM64 Android fuzzer
-    cd $SRC/buttercup_target/scripts
-    export AFL_PATH=/src/aflplusplus
-    export NDK=/src/android-ndk-r25c
+# Build the ARM64 Android fuzzer
+cd $SRC/buttercup_target/scripts
+export AFL_PATH=/src/aflplusplus
+export NDK=/src/android-ndk-r25c
 
-    sed -i 's|#include "afl-fuzz.h"||' ../harnesses/fuzz_image_processing.c
-  
-    ./build.sh afl
-  
-    # Copy the fuzzer to the output directory
-    cp ../build/fuzz_image_processing_afl $OUT/fuzz_image_processing
-  
-    # Copy the pre-compiled .so since the harness dlopens it
-    cp ../lib/libimage_processing_util_jni.so $OUT/
+sed -i 's|#include "afl-fuzz.h"||' ../harnesses/fuzz_image_processing.c
+	
+# ADD THIS LINE TO CREATE THE BUILD DIRECTORY
+mkdir -p ../build
+
+     ./build.sh afl
+
+# Copy the fuzzer to the output directory
+cp ../build/fuzz_image_processing_afl $OUT/fuzz_image_processing
+
+# Copy the pre-compiled .so since the harness dlopens it
+cp ../lib/libimage_processing_util_jni.so $OUT/
